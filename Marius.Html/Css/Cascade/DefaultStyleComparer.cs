41d0c02cd39d930731bab885ba15a829dc587e35
marius.klimantavicius@gmail.com <marius.klimantavicius@gmail.com@557dcb64-6c96-51a6-ecd1-ec77abff28d1>
2010-06-18 07:51:30 +0000

    Cascade (draft implementation)

---
+++ Marius.Html/Css/Cascade/DefaultStyleComparer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marius.Html.Css.Dom;
+
+namespace Marius.Html.Css.Cascade
+{
+    public class DefaultStyleComparer: IComparer<CssPreparedStyle>
+    {
+        public static readonly DefaultStyleComparer Instance = new DefaultStyleComparer();
+
+        public int Compare(CssPreparedStyle x, CssPreparedStyle y)
+        {
+            if (object.ReferenceEquals(x, y))
+                return 0;
+
+            int xweight, yweight;
+            xweight = Importance(x);
+            yweight = Importance(y);
+
+            if (xweight != yweight)
+                return -(xweight - yweight);
+
+            int speccomp = x.Selector.Specificity.CompareTo(y.Selector.Specificity);
+            if (speccomp != 0)
+                return -speccomp;
+
+            return -(x.Index - y.Index);
+        }
+
+        private int Importance(CssPreparedStyle s)
+        {
+            /*
+               1 1. user agent declarations
+               2 1.1 user agent important
+               3 2. user normal declarations
+               4 3. author normal declarations
+               5 4. author important declarations
+               6 5. user important declarations 
+            */
+            switch (s.Source)
+            {
+                case CssStylesheetSource.Agent:
+                    if (s.IsImportant)
+                        return 2;
+                    return 1;
+                case CssStylesheetSource.Author:
+                    if (s.IsImportant)
+                        return 5;
+                    return 4;
+                case CssStylesheetSource.User:
+                    if (s.IsImportant)
+                        return 6;
+                    return 3;
+            }
+            throw new CssInvalidStateException();
+        }
+    }
+}
