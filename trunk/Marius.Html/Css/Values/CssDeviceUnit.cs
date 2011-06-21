c7bdf18690779e0424246f61c0ff24ff181afbbf
marius.klimantavicius@gmail.com <marius.klimantavicius@gmail.com@557dcb64-6c96-51a6-ecd1-ec77abff28d1>
2011-06-21 04:56:42 +0000

    Addind Device units as a solution to various Css units. Removing layout stuff to redo

---
+++ trunk/Marius.Html/Css/Values/CssDeviceUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Marius.Html.Css.Values
+{
+    /// <summary>
+    /// Represents a device unit (all css length values are converted to device units).
+    /// It is used to render as well (can be pixels, sub-pixels and so on...)
+    /// </summary>
+    public sealed class CssDeviceUnit
+    {
+        public double Value { get; private set; }
+
+        public CssDeviceUnit(double value)
+        {
+            Value = value;
+        }
+    }
+}
