2b1828bf1f4c5d7ecc554d1307e198e635fd6691
marius.klimantavicius@gmail.com <marius.klimantavicius@gmail.com@557dcb64-6c96-51a6-ecd1-ec77abff28d1>
2011-09-14 17:37:26 +0000

    Starting to work on layout (currently prototyping possible layout engine). In later stages will most likely have to rewrite cascade and box generation

---
+++ Marius.Html/Css/Layout/CssLayoutContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marius.Html.Css.Values;
+
+namespace Marius.Html.Css.Layout
+{
+    public abstract class CssLayoutContext
+    {
+        public abstract CssDeviceUnit Width { get; }
+        public abstract CssDeviceUnit Height { get; }
+
+        public abstract CssDeviceUnit ToDeviceUnits(CssValue value);
+    }
+}
